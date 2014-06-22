2c6d89e933808fc5aa0ceed79ae30d064daea014
Marcus Völker <marcus@upvoid.com>
2014-06-22 20:59:38 +0200

    HLint fixes

--- sat.hs
+++ sat.hs
@@ -8,13 +8,13 @@ type CNF = [Clause]
 
 simplifyClause :: Clause -> Int -> Bool -> Maybe Clause
 simplifyClause [] _ _ = Just []
-simplifyClause (x:xs) y b | (extract x) /= y = (simplifyClause xs y b) >>= (\k -> Just (x:k))
-simplifyClause ((P x):xs) _ True = Nothing
-simplifyClause ((N x):xs) _ False = Nothing
+simplifyClause (x:xs) y b | extract x /= y = simplifyClause xs y b >>= (\k -> Just (x:k))
+simplifyClause (P _:_) _ True = Nothing
+simplifyClause (N _:_) _ False = Nothing
 simplifyClause (_:xs) y b = simplifyClause xs y b
 
 simplifyFormula :: CNF -> Int -> Bool -> CNF
-simplifyFormula cnf x b = catMaybes (map (\k -> simplifyClause k x b) cnf)
+simplifyFormula cnf x b = mapMaybe (\k -> simplifyClause k x b) cnf
 
 unitPropagate :: CNF -> Clause -> CNF
 unitPropagate x [P l] = simplifyFormula x l True
@@ -29,11 +29,11 @@ pureLiteralAssign' f l | not (isPure f l) = f
 pureLiteralAssign' f l = purge f l
 
 purge :: CNF -> Literal -> CNF
-purge f l = filter (\c -> notElem l c) f
+purge f l = filter (notElem l) f
 
 isPure :: CNF -> Literal -> Bool
-isPure f (P x) = notElem (N x) (concat f)
-isPure f (N x) = notElem (P x) (concat f)
+isPure f (P x) = N x `notElem` concat f
+isPure f (N x) = P x `notElem` concat f
 
 chooseLiteral :: CNF -> Int
 chooseLiteral f | null (concat f) = 0
@@ -46,15 +46,9 @@ extract (N x) = x
 dpll :: CNF -> Bool
 dpll [] = True
 dpll x | any null x = False
-dpll x | null x' = True
-  where
-    x' = foldl pureLiteralAssign up up 
-    up = foldl unitPropagate x x 
-dpll x | any null x' = False
-  where
-    x' = foldl pureLiteralAssign up up 
-    up = foldl unitPropagate x x 
-dpll x = dpll ([P l] : x') || dpll ([N l] : x')
+       | null x' = True
+       | any null x' = False
+       | otherwise   = dpll ([P l] : x') || dpll ([N l] : x')
   where
     l  = chooseLiteral x'
     x' = foldl pureLiteralAssign up up 
